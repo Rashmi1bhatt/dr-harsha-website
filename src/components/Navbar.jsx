8e31e0e6f619538020148e1842a380a848db55bd
Rashmi1bhatt <32875325+Rashmi1bhatt@users.noreply.github.com>
2026-04-20 11:36:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,15 @@
+import "./Navbar.css";
 
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Dr. Harsha</h2>
+
+      <div className="nav-links">
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
