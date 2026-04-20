fd07d68d2d6984faa09a98d06fd176a0ce5202ef
Rashmi1bhatt <32875325+Rashmi1bhatt@users.noreply.github.com>
2026-04-20 11:40:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div style={{ paddingTop: "70px" }}>
+        <Hero />
+        <About />
+        <Services />
+        <Contact />
+      </div>
+    </>
+  );
+}
+
+export default App;
 
 export default function App() {
   const kauveryLink =
